74b91c8e48db0b5cbac77a255c97499d24ce6d13
Zach <zmommaerts@gmail.com>
2022-01-10 16:21:28 -0600

    stuck

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,11 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = Student.all.where('first_name=? OR last_name=?', params[:search])
     render json: students
   end
 
+  def show
+    render json: Student.find_by_id(params[:id])
+  end
 end
